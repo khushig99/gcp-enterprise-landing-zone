c1f52cee5e0538062dd06ec75eee063e843b5e44
Khushi <khushig080803@gmail.com>
2026-06-14 15:58:17 +0000

    added files

---
+++ environments/bootstrap/main.tf
@@ -0,0 +1,25 @@
+locals {
+  bootstrap_project = "lz-security"
+}
+
+module "state_bucket" {
+
+  source = "../../modules/state-bucket"
+
+  bucket_name = "khushi-tf-state"
+}
+
+module "terraform_sa" {
+
+  source = "../../modules/terraform-sa"
+}
+
+resource "google_organization_iam_member" "terraform_folder_admin" {
+
+  org_id = local.org_id
+
+  role = "roles/resourcemanager.folderAdmin"
+
+  member = "serviceAccount:${module.terraform_sa.email}"
+}
+
