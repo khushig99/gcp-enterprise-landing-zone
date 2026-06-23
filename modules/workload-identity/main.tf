3c97e340be84168314de6e8559bc043e11d4d79d
Khushi Gupta <104582212+khushig99@users.noreply.github.com>
2026-06-23 19:35:13 +0530

    Update main.tf

--- modules/workload-identity/main.tf
+++ modules/workload-identity/main.tf
@@ -26,11 +26,9 @@ resource "google_iam_workload_identity_pool_provider" "github" {
 
 resource "google_service_account_iam_member" "github" {
 
-  service_account_id =
-    "projects/-/serviceAccounts/${var.terraform_sa_email}"
+  service_account_id = "projects/-/serviceAccounts/${var.terraform_sa_email}"
 
   role = "roles/iam.workloadIdentityUser"
 
-  member =
-    "principalSet://iam.googleapis.com/${google_iam_workload_identity_pool.github.name}/attribute.repository/${var.github_repository}"
+  member = "principalSet://iam.googleapis.com/${google_iam_workload_identity_pool.github.name}/attribute.repository/${var.github_repository}"
 }
