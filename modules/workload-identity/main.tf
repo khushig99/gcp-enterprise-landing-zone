c42f067258ab32cb97c401f0d5b5cd7e525f23c3
Khushi Gupta <104582212+khushig99@users.noreply.github.com>
2026-06-23 17:56:37 +0530

    Update main.tf

--- modules/workload-identity/main.tf
+++ modules/workload-identity/main.tf
@@ -5,6 +5,25 @@ resource "google_iam_workload_identity_pool" "github" {
   display_name = "GitHub Actions Pool"
 }
 
+resource "google_iam_workload_identity_pool_provider" "github" {
+
+  workload_identity_pool_id = google_iam_workload_identity_pool.github.workload_identity_pool_id
+
+  workload_identity_pool_provider_id = "github-provider"
+
+  display_name = "GitHub Actions Provider"
+
+  attribute_mapping = {
+    "google.subject"       = "assertion.sub"
+    "attribute.actor"      = "assertion.actor"
+    "attribute.repository" = "assertion.repository"
+  }
+
+  oidc {
+    issuer_uri = "https://token.actions.githubusercontent.com"
+  }
+}
+
 resource "google_service_account_iam_member" "github" {
 
   service_account_id =
