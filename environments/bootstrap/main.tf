75efd4fd6538437b5df83e99f2aa6f6694499be2
Khushi Gupta <104582212+khushig99@users.noreply.github.com>
2026-06-23 19:28:06 +0530

    Update main.tf

--- environments/bootstrap/main.tf
+++ environments/bootstrap/main.tf
@@ -1,12 +1,14 @@
-module "state_bucket" {
+locals {
+  bootstrap_project = "project-fce939dd-3922-4178-b0f"
+}
 
+module "state_bucket" {
   source = "../../modules/state-bucket"
 
-  bucket_name = "khushi-tf-state"
+  bucket_name = "khushi-tf-state-423969081969"
 }
 
 module "terraform_sa" {
-
   source = "../../modules/terraform-sa"
 }
 
@@ -14,16 +16,13 @@ module "workload_identity" {
   source = "../../modules/workload-identity"
 
   terraform_sa_email = module.terraform_sa.email
-
   github_repository  = "khushig99/gcp-enterprise-landing-zone"
 }
 
 resource "google_organization_iam_member" "terraform_folder_admin" {
-
-  org_id = 423969081969
+  org_id = "423969081969"
 
   role = "roles/resourcemanager.folderAdmin"
 
   member = "serviceAccount:${module.terraform_sa.email}"
 }
-
