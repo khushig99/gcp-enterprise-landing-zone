d13cfe94d4fdc013359ff768cd383af27dfa3aa1
Khushi Gupta <104582212+khushig99@users.noreply.github.com>
2026-06-23 16:49:10 +0530

    Update providers.tf

---
+++ environments/bootstrap/providers.tf
@@ -0,0 +1,14 @@
+terraform {
+  required_version = ">= 1.6"
+
+  required_providers {
+    google = {
+      source  = "hashicorp/google"
+      version = "~> 6.0"
+    }
+  }
+}
+
+provider "google" {
+  project = "project-fce939dd-3922-4178-b0f"
+}
