0f6ec76311c821a82a5b024fbdc8fb325ad6a2f7
Khushi Gupta <104582212+khushig99@users.noreply.github.com>
2026-06-23 21:07:51 +0530

    Update main.tf

--- modules/workload-identity/main.tf
+++ modules/workload-identity/main.tf
@@ -1,17 +1,31 @@
+resource "google_iam_workload_identity_pool" "github" {
+  workload_identity_pool_id = "github-pool"
+  display_name              = "GitHub Actions Pool"
+}
+
 resource "google_iam_workload_identity_pool_provider" "github" {
-
-  workload_identity_pool_id = google_iam_workload_identity_pool.github.workload_identity_pool_id
-
+  workload_identity_pool_id          = google_iam_workload_identity_pool.github.workload_identity_pool_id
   workload_identity_pool_provider_id = "github-provider"
 
   display_name = "GitHub Actions Provider"
 
   attribute_mapping = {
     "google.subject"       = "assertion.sub"
+    "attribute.actor"      = "assertion.actor"
     "attribute.repository" = "assertion.repository"
   }
+
+  attribute_condition = "assertion.repository != ''"
 
   oidc {
     issuer_uri = "https://token.actions.githubusercontent.com"
   }
+}
+
+resource "google_service_account_iam_member" "github" {
+  service_account_id = "projects/-/serviceAccounts/${var.terraform_sa_email}"
+
+  role = "roles/iam.workloadIdentityUser"
+
+  member = "principalSet://iam.googleapis.com/${google_iam_workload_identity_pool.github.name}/attribute.repository/${var.github_repository}"
 }
