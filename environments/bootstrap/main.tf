769acbded41a635b716557348bf26776aa6faab4
Khushi Gupta <104582212+khushig99@users.noreply.github.com>
2026-06-23 17:46:40 +0530

    Update main.tf

--- environments/bootstrap/main.tf
+++ environments/bootstrap/main.tf
@@ -14,6 +14,14 @@ module "terraform_sa" {
   source = "../../modules/terraform-sa"
 }
 
+module "workload_identity" {
+  source = "../../modules/workload-identity"
+
+  terraform_sa_email = module.terraform_sa.email
+
+  github_repository  = ""khushig99/gcp-enterprise-landing-zone"
+}
+
 resource "google_organization_iam_member" "terraform_folder_admin" {
 
   org_id = 423969081969
